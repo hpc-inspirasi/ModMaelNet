c9723f706acf361b88cf2cba136743d3a2a0506a
Ari Mogi <arimogi@yahoo.com>
2024-11-14 13:48:56 -0800

    Add MSL run Script.

---
+++ run_msl.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+bash ./scripts/NEW_RL/MSL_scripts/train_anomaly_transformer_fast_learner.sh
+#bash ./scripts/NEW_RL/MSL_scripts/train_dcdetector_fast_learner.sh
+#bash ./scripts/NEW_RL/MSL_scripts/train_maelnet_slow_learner.sh
--- run_swat.sh
+++ run_swat.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 date.exe >> start1.txt
 bash ./scripts/NEW_RL/SWaT_scripts/train_anomaly_transformer_fast_learner.sh
-date.exe >> start2.txt
-bash ./scripts/NEW_RL/SWaT_scripts/train_dcdetector_fast_learner.sh
-date.exe >> start3.txt
-bash ./scripts/NEW_RL/SWaT_scripts/train_maelnet_slow_learner.sh
+#date.exe >> start2.txt
+#bash ./scripts/NEW_RL/SWaT_scripts/train_dcdetector_fast_learner.sh
+#date.exe >> start3.txt
+#bash ./scripts/NEW_RL/SWaT_scripts/train_maelnet_slow_learner.sh
 date.exe >> finish.txt
