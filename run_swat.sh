3ae123d1bd27916f9dada43c7643c1f3a26fb7d9
Ari Mogi <arimogi@yahoo.com>
2024-11-15 05:49:30 -0800

    Fix the run script.

--- run_swat.sh
+++ run_swat.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
-date.exe >> start1.txt
-bash ./scripts/NEW_RL/SWaT_scripts/train_anomaly_transformer_fast_learner.sh
+echo "Process 1: " && date.exe >> waktu.txt && sleep 4 &
+PID1 = $$
+echo "PID 1: " + $PID1
+wait $PID1
+echo "Process 2: " && date.exe >> waktu.txt && sleep 4 &
+PID2 = $$
+wait $PID2
+#bash ./scripts/NEW_RL/SWaT_scripts/train_anomaly_transformer_fast_learner.sh
 #date.exe >> start2.txt
 #bash ./scripts/NEW_RL/SWaT_scripts/train_dcdetector_fast_learner.sh
 #date.exe >> start3.txt
 #bash ./scripts/NEW_RL/SWaT_scripts/train_maelnet_slow_learner.sh
-date.exe >> finish.txt
+date.exe >> waktu.txt
