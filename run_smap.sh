a26aa1e00f714c2138f2d8c4f479d77c0f67f45c
Ari Mogi <arimogi@yahoo.com>
2024-11-14 17:41:29 -0800

    Add SMAP run script.

--- run_smap.sh
+++ run_smap.sh
@@ -1,3 +1,4 @@
-bash ./scripts/NEW_RL/SWaT_scripts/train_anomaly_transformer_fast_learner.sh
-bash ./scripts/NEW_RL/SWaT_scripts/train_dcdetector_fast_learner.sh
-bash ./scripts/NEW_RL/SWaT_scripts/train_maelnet_slow_learner.sh
+#!/usr/bin/bash
+bash ./scripts/NEW_RL/SMAP_scripts/train_anomaly_transformer_fast_learner.sh &
+bash ./scripts/NEW_RL/SMAP_scripts/train_dcdetector_fast_learner.sh &
+bash ./scripts/NEW_RL/SMAP_scripts/train_maelnet_slow_learner.sh &
