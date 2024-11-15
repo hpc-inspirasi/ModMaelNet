ebf111b3516dc7d2a7ce6db8f850c990b8f9d862
Ari Mogi <arimogi@yahoo.com>
2024-11-14 17:44:25 -0800

    Change py to python syntax.

--- scripts/NEW_RL/SMAP_scripts/test.sh
+++ scripts/NEW_RL/SMAP_scripts/test.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
 
-py -u run_anomaly.py \
+python -u run_anomaly.py \
   --is_training 0 \
   --root_path ./dataset/SMAP/ \
   --model_id MaelNetS2_AnomalyTransformer_DCDetector_RL_TA\
