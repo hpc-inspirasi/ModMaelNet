4f02bc0dd6337743c1a36dd2c1540946f51debb5
HPC Inspirasi <hpc-inspirasi@outlook.com>
2024-11-28 07:08:50 +0800

    Add only train script for SWaT dataset.

--- scripts/NEW_RL/SWaT_scripts/run-train.sh
+++ scripts/NEW_RL/SWaT_scripts/run-train.sh
@@ -2,7 +2,7 @@ export DATASET_SC="SWaT"
 export DIR_RES='./results/'
 export FL_RES="${DIR_RES}run_${DATASET_SC}.txt"
 echo " " >> $FL_RES
-echo "========================================> run on: `date`" >> $FL_RES
+echo "Training only ===========================> run on: `date`" >> $FL_RES
 echo "Set result file in ${FL_RES}"
 #
 echo "Script MaelNET on Database $DATASET_SC" >> $FL_RES
@@ -11,21 +11,15 @@ echo "Started at `date`" >> $FL_RES
 bash ./scripts/NEW_RL/${DATASET_SC}_scripts/train_anomaly_transformer_fast_learner.sh
 echo "Finished at `date`" >> $FL_RES
 echo "---"
-
+#
 echo "Train DC Detector" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
 bash ./scripts/NEW_RL/${DATASET_SC}_scripts/train_dcdetector_fast_learner.sh
 echo "Finished at `date`" >> $FL_RES
 echo "---"
-
+#
 echo "Train Slow Learner" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
 bash ./scripts/NEW_RL/${DATASET_SC}_scripts/train_maelnet_slow_learner.sh
 echo "Finished at `date`" >> $FL_RES
-echo "---"
-
-echo "Testing Model" >> $FL_RES
-echo "Started at `date`" >> $FL_RES
-bash ./scripts/NEW_RL/${DATASET_SC}_scripts/test.sh
-echo "Finished at `date`" >> $FL_RES
-echo "----------------------------" >> $FL_RES
+echo "----------------------------------------> Training done on: `date`" >> $FL_RES
