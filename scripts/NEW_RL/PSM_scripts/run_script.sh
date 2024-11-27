5d75d01e23ba5951c84cefa4fd11a6a268ffa13d
HPC Inspirasi <hpc-inspirasi@outlook.com>
2024-11-27 11:07:09 +0800

    Delete the old run scripts.

--- scripts/NEW_RL/PSM_scripts/run_script.sh
+++
@@ -1,4 +0,0 @@
-./scripts/NEW_RL/PSM_scripts/train_anomaly_transformer_fast_learner.sh
-./scripts/NEW_RL/PSM_scripts/train_dcdetector_fast_learner.sh
-./scripts/NEW_RL/PSM_scripts/train_maelnet_slow_learner.sh
-./scripts/NEW_RL/PSM_scripts/test.sh
