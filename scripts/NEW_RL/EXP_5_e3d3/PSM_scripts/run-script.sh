fcdb426e73b972ea594cfb239d025d4879de73e8
HPC Inspirasi <hpc-inspirasi@outlook.com>
2024-12-14 10:27:35 +0800

    Update running script for EXP_3, EXP_4, EXP_5, and EXP_6.

--- scripts/NEW_RL/EXP_5_e3d3/PSM_scripts/run-script.sh
+++ scripts/NEW_RL/EXP_5_e3d3/PSM_scripts/run-script.sh
@@ -1,6 +1,9 @@
 export DATASET_SC="PSM"
 export DIR_RES='./results/'
+export FL_EXP="EXP_5_e3d3"
+
 export FL_RES="${DIR_RES}run_${DATASET_SC}.txt"
+export RUN_RES="${DIR_RES}result_test_${DATASET_SC}.txt"
 echo "========================================" >> $FL_RES
 echo "Set result file in ${FL_RES}"
 echo " " >> $FL_RES
@@ -8,24 +11,24 @@ echo " " >> $FL_RES
 echo "Script MaelNET on Database $DATASET_SC" >> $FL_RES
 echo "Train Anomaly" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
-bash ./scripts/NEW_RL/${DATASET_SC}_scripts/train_anomaly_transformer_fast_learner.sh
+bash ./scripts/NEW_RL/${FL_EXP}/${DATASET_SC}_scripts/train_anomaly_transformer_fast_learner.sh
 echo "Finished at `date`" >> $FL_RES
 echo "---"
 
 echo "Train DC Detector" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
-bash ./scripts/NEW_RL/${DATASET_SC}_scripts/train_dcdetector_fast_learner.sh
+bash ./scripts/NEW_RL/${FL_EXP}/${DATASET_SC}_scripts/train_dcdetector_fast_learner.sh
 echo "Finished at `date`" >> $FL_RES
 echo "---"
 
 echo "Train Slow Learner" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
-bash ./scripts/NEW_RL/${DATASET_SC}_scripts/train_maelnet_slow_learner.sh
+bash ./scripts/NEW_RL/${FL_EXP}/${DATASET_SC}_scripts/train_maelnet_slow_learner.sh
 echo "Finished at `date`" >> $FL_RES
 echo "---"
 
 echo "Testing Model" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
-bash ./scripts/NEW_RL/${DATASET_SC}_scripts/test.sh
+bash ./scripts/NEW_RL/${FL_EXP}/${DATASET_SC}_scripts/test.sh >> $RUN_RES
 echo "Finished at `date`" >> $FL_RES
 echo "----------------------------" >> $FL_RES
--- scripts/NEW_RL/EXP_6_e4d3/SWaT_scripts/run-train.sh
+++ scripts/NEW_RL/EXP_6_e4d3/SWaT_scripts/run-train.sh
@@ -1,26 +1,34 @@
 export DATASET_SC="SWaT"
 export DIR_RES='./results/'
-export EXP_NO='EXP_3'
+export FL_EXP="EXP_6_e4d3"
+
 export FL_RES="${DIR_RES}run_${DATASET_SC}.txt"
-echo " " >> $FL_RES
-echo "Training only ===========================> run on: `date`" >> $FL_RES
+export RUN_RES="${DIR_RES}result_test_${DATASET_SC}.txt"
+echo "========================================" >> $FL_RES
 echo "Set result file in ${FL_RES}"
-#
+echo " " >> $FL_RES
+
 echo "Script MaelNET on Database $DATASET_SC" >> $FL_RES
 echo "Train Anomaly" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
-bash ./scripts/NEW_RL/${EXP_NO}/${DATASET_SC}_scripts/train_anomaly_transformer_fast_learner.sh
+bash ./scripts/NEW_RL/${FL_EXP}/${DATASET_SC}_scripts/train_anomaly_transformer_fast_learner.sh
 echo "Finished at `date`" >> $FL_RES
 echo "---"
-#
+
 echo "Train DC Detector" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
-bash ./scripts/NEW_RL/${EXP_NO}/${DATASET_SC}_scripts/train_dcdetector_fast_learner.sh
+bash ./scripts/NEW_RL/${FL_EXP}/${DATASET_SC}_scripts/train_dcdetector_fast_learner.sh
 echo "Finished at `date`" >> $FL_RES
 echo "---"
-#
+
 echo "Train Slow Learner" >> $FL_RES
 echo "Started at `date`" >> $FL_RES
-bash ./scripts/NEW_RL/${EXP_NO}/${DATASET_SC}_scripts/train_maelnet_slow_learner.sh
+bash ./scripts/NEW_RL/${FL_EXP}/${DATASET_SC}_scripts/train_maelnet_slow_learner.sh
 echo "Finished at `date`" >> $FL_RES
-echo "----------------------------------------> Training done on: `date`" >> $FL_RES
+echo "---"
+
+# echo "Testing Model" >> $FL_RES
+# echo "Started at `date`" >> $FL_RES
+# bash ./scripts/NEW_RL/${FL_EXP}/${DATASET_SC}_scripts/test.sh >> $RUN_RES
+# echo "Finished at `date`" >> $FL_RES
+# echo "----------------------------" >> $FL_RES
